a7c686be0644d8e407dc8653f29575c34fd176d7
Jojin <jojinjoseph@gmail.com>
2025-12-17 21:55:42 +0400

    examples

--- PythonLearning/in_if_elif_Example.py
+++ PythonLearning/in_if_elif_Example.py
@@ -1,7 +1,9 @@
 str = input("Type some String : ")
-if "add" in str:
+if "add" in str or "new" in str:
     print("Found add")
 elif "show" in str:
     print("Found Show")
 else :
     print("Found nothing")
+
+print(str[5:])
